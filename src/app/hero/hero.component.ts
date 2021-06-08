d176d15fd4d742b43b75940baecf93e9ce1cf716
mbalandari <m.balandari.toroghi@gmail.com>
2021-06-08 09:10:12 +0300

    Tue-01

---
+++ src/app/hero/hero.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-hero',
+  templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.css']
+})
+export class HeroComponent implements OnInit {
+  hero = 'Windstorm';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
